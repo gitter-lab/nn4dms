3430f61c6963c3b3c09874b30a8fc947becf3002
Sam Gelman <samgelman1@gmail.com>
2020-09-06 17:09:55 -0500

    add new way of defining datasets to constants, still need to update code to actually use it

--- code/constants.py
+++ code/constants.py
@@ -2,6 +2,53 @@
 
 DS_NAMES = ["avgfp", "bgl3", "gb1", "pab1", "ube4b"]
 
+DATASETS = {"avgfp": {"ds_name": "avgfp",
+                      "ds_dir": "data/avgfp",
+                      "ds_fn": "data/avgfp/avgfp.tsv",
+                      "wt_aa": "SKGEELFTGVVPILVELDGDVNGHKFSVSGEGEGDATYGKLTLKFICTTGKLPVPWPTL"
+                               "VTTLSYGVQCFSRYPDHMKQHDFFKSAMPEGYVQERTIFFKDDGNYKTRAEVKFEGDTL"
+                               "VNRIELKGIDFKEDGNILGHKLEYNYNSHNVYIMADKQKNGIKVNFKIRHNIEDGSVQL"
+                               "ADHYQQNTPIGDGPVLLPDNHYLSTQSALSKDPNEKRDHMVLLEFVTAAGITHGMDELYK",
+                      "wt_ofs": 0,
+                      "pdb_fn": "data/avgfp/avgfp_rosetta_model.pdb"},
+
+            "bgl3": {"ds_name": "bgl3",
+                     "ds_dir": "data/bgl3",
+                     "ds_fn": "data/bgl3/bgl3.tsv",
+                     "wt_aa": "MVPAAQQTAMAPDAALTFPEGFLWGSATASYQIEGAAAEDGRTPSIWDTYARTPGRVRNGDTGDVAT"
+                              "DHYHRWREDVALMAELGLGAYRFSLAWPRIQPTGRGPALQKGLDFYRRLADELLAKGIQPVATLYHWD"
+                              "LPQELENAGGWPERATAERFAEYAAIAADALGDRVKTWTTLNEPWCSAFLGYGSGVHAPGRTDPVAALR"
+                              "AAHHLNLGHGLAVQALRDRLPADAQCSVTLNIHHVRPLTDSDADADAVRRIDALANRVFTGPMLQGAYP"
+                              "EDLVKDTAGLTDWSFVRDGDLRLAHQKLDFLGVNYYSPTLVSEADGSGTHNSDGHGRSAHSPWPGADRVA"
+                              "FHQPPGETTAMGWAVDPSGLYELLRRLSSDFPALPLVITENGAAFHDYADPEGNVNDPERIAYVRDHLAAV"
+                              "HRAIKDGSDVRGYFLWSLLDNFEWAHGYSKRFGAVYVDYPTGTRIPKASARWYAEVARTGVLPTAGDPNSSS"
+                              "VDKLAAALEHHHHHH",
+                     "wt_ofs": 0,
+                     "pdb_fn": "data/bgl3/bgl3_rosetta_model.pdb"},
+
+            "gb1": {"ds_name": "gb1",
+                    "ds_dir": "data/gb1",
+                    "ds_fn": "data/gb1/gb1.tsv",
+                    "wt_aa": "MQYKLILNGKTLKGETTTEAVDAATAEKVFKQYANDNGVDGEWTYDDATKTFTVTE",
+                    "wt_ofs": 0,
+                    "pdb_fn": "data/gb1/2qmt.pdb"},
+
+            "pab1": {"ds_name": "pab1",
+                     "ds_dir": "data/pab1",
+                     "ds_fn": "data/pab1/pab1.tsv",
+                     "wt_aa": "GNIFIKNLHPDIDNKALYDTFSVFGDILSSKIATDENGKSKGFGFVHFEEEGAAKEAIDALNGMLLNGQEIYVAP",
+                     "wt_ofs": 126,
+                     "pdb_fn": "data/pab1/pab1_rosetta_model.pdb"},
+
+            "ube4b": {"ds_name": "ube4b",
+                      "ds_dir": "data/ube4b",
+                      "ds_fn": "data/ube4b/ube4b.tsv",
+                      "wt_aa": "IEKFKLLAEKVEEIVAKNARAEIDYSDAPDEFRDPLMDTLMTDPVRLP"
+                               "SGTVMDRSIILRHLLNSPTDPFNRQMLTESMLEPVPELKEQIQAWMREKQSSDH",
+                      "wt_ofs": 0,
+                      "pdb_fn": "data/ube4b/ube4b_rosetta_model.pdb"}}
+
+
 DS_DIRS = {"avgfp": "data/avgfp",
            "bgl3": "data/bgl3",
            "gb1": "data/gb1",
