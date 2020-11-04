c8cb0cf9a42e45628cc6220e53862c60245ab5e0
Sam Gelman <samgelman1@gmail.com>
2020-11-04 15:50:23 -0600

    Better support for getting network outputs at internal layers and fix tensorflow ops being recreated on multiple calls to run_inference()

--- code/inference.py
+++ code/inference.py
@@ -38,13 +38,21 @@ def run_inference(encoded_data, ckpt_prefix_fn=None, sess=None, output_tensor_na
     # predictions come from "output/BiasAdd:0" which refers to the final fully connected layer of the network
     # after the bias has been added. all my networks will have this same "output" layer. the extra squeeze
     # operation is added for data formatting
-    predictions = tf.squeeze(sess.graph.get_tensor_by_name(output_tensor_name))
+    predictions = sess.graph.get_tensor_by_name(output_tensor_name)
 
     # auto batch
     if encoded_data.shape[0] > batch_size:
+
+        # run a single example through the network to get the shape of the output
+        out_shape = list(np.squeeze(sess.run(predictions, feed_dict={input_seqs_ph: encoded_data[0:1, ...]})).shape)
+
+        # set up an array to store outputs from each batch
+        predictions_for_data = np.zeros([encoded_data.shape[0]] + out_shape)
+
+        # compute the number of batches and split data into batches using the batch generator
         num_batches = int(np.ceil(encoded_data.shape[0]/batch_size))
-        predictions_for_data = np.zeros(encoded_data.shape[0])
         bg = batch_generator([encoded_data], batch_size, skip_last_batch=False, num_epochs=1, shuffle=False)
+
         for batch_num, batch_data in tqdm(enumerate(bg), total=num_batches):
             # if batch_num % 10 == 0:
             #     print("Running batch {} of {}...".format(batch_num+1, num_batches))
@@ -52,9 +60,9 @@ def run_inference(encoded_data, ckpt_prefix_fn=None, sess=None, output_tensor_na
             predictions_for_batch = sess.run(predictions, feed_dict={input_seqs_ph: batch_data})
             start_index = batch_num * batch_size
             end_index = start_index + batch_size
-            predictions_for_data[start_index:end_index] = predictions_for_batch
+            predictions_for_data[start_index:end_index, ...] = np.squeeze(predictions_for_batch)
     else:
-        predictions_for_data = sess.run(predictions, feed_dict={input_seqs_ph: encoded_data})
+        predictions_for_data = np.squeeze(sess.run(predictions, feed_dict={input_seqs_ph: encoded_data}))
 
     if created_sess:
         sess.close()
