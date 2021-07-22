36b4d4b573ccf72967f2e5b806787e4722a84bb1
Sam Gelman <samgelman1@gmail.com>
2021-07-22 17:04:32 -0500

    fix token replacement bug and add support for mutation and position-based split directories

--- code/gen_args.py
+++ code/gen_args.py
@@ -1,9 +1,11 @@
 """ for generating arguments files for regression.py
     can be used for reduced splits, hyperparameter sweeps, HTCondor runs, etc """
 
-from os.path import join, basename, isfile
+import os
+from os.path import join, basename, isfile, isdir
 import itertools
 import argparse
+import re
 
 import yaml
 
@@ -11,12 +13,20 @@ from parse_reg_args import save_args
 import utils
 
 
-def parse_yml(yml_fn):
+def parse_yml(yml_fn, expand_split_dirs=False):
     """ parses a yml file, which may contain lists for certain arguments, to potentially
         multiple dictionaries with no lists """
 
     with open(yml_fn, "r") as f:
         yml = yaml.safe_load(f)
+
+        # expand the reduced split dirs if requested, so that each split dir will have its own job
+        # this is basically just so I don't have to manually specify all the split dirs for a big run in the
+        # run file. although manually doing it would probably be best since I don't plan on using this feature
+        # in the future... oh well.
+        if expand_split_dirs:
+            yml["split_dir"] = expand_reduced_split_dirs(yml["split_dir"])
+
         # get the possible options for each arg, and make sure it's a list, even if it's just one possible option
         options = [val if isinstance(val, list) else [val] for val in yml.values()]
         combinations = list(itertools.product(*options))
@@ -27,12 +37,59 @@ def parse_yml(yml_fn):
             d = dict(zip(yml.keys(), combo))
             dicts.append(d)
 
+    # perform replacements of key tokens
+    for args_dict in dicts:
+        for k, v in args_dict.items():
+            if isinstance(v, str):
+                # search for any tokens for replacement in all string args
+                rep_tokens = re.findall("{.*?}", v)
+                for token in rep_tokens:
+                    # needs to be a corresponding argument for each token
+                    # user can specify normal program arguments or special arguments in the form of #argument#
+                    # arguments in the form of {argument} get deleted and are not in the final generated args file
+                    if token in args_dict.keys():
+                        rep_value = args_dict[token]
+                    elif token[1:-1] in args_dict.keys():
+                        rep_value = args_dict[token[1:-1]]
+                    else:
+                        raise ValueError("couldn't find specified token {} in args {}".format(token, args_dict))
+                    # perform the replacement
+                    args_dict[k] = v.replace(token, rep_value)
+            #
+            # # this can be generalized for any token, but we are only using dataset_name for now
+            # if isinstance(v, str) and "#dataset_name#" in v:
+            #     args_dict[k] = v.replace("#dataset_name#", args_dict["dataset_name"])
+
+    # args in the form of {argument} are just there for token replacements, so they can be removed
+    for args_dict in dicts:
+        for k, v in args_dict.items():
+            if k.startswith("{") and k.endswith("}"):
+                del args_dict[k]
+
     return dicts
 
 
-def gen_args_from_template_fn(template_fn, out_dir):
+def expand_reduced_split_dirs(reduced_split_dirs):
+    # depending on how the split_dir was specified in the yaml file, we might be getting a single string or a list
+    if isinstance(reduced_split_dirs, str):
+        reduced_split_dirs = [reduced_split_dirs]
+
+    all_split_dirs = []
+    for rsd in reduced_split_dirs:
+        fns = [join(rsd, x) for x in os.listdir(rsd)]
+        # is this a directory containing split dirs, or actual splits? if split dirs, then append each to the new list
+        # add an exception for "additional_info" dirs in splits that contain it (mutation-based, position-based)
+        if isdir(fns[0]) and basename(fns[0]) != "additional_info":
+            for split_dir in fns:
+                all_split_dirs.append(split_dir)
+        else:
+            all_split_dirs.append(rsd)
+    return all_split_dirs
+
+
+def gen_args_from_template_fn(template_fn, out_dir, expand_split_dirs=False):
     utils.mkdir(out_dir)
-    args_dicts = parse_yml(template_fn)
+    args_dicts = parse_yml(template_fn, expand_split_dirs=expand_split_dirs)
     for i, d in enumerate(args_dicts):
         save_args(d, join(out_dir, "{}.txt".format(i)))
 
@@ -43,7 +100,7 @@ def main(args):
         raise FileNotFoundError("couldn't find template file: {}".format(args.f))
 
     out_dir = args.out_dir if args.out_dir != "" else join("regression_args", basename(args.f).split(".")[0])
-    gen_args_from_template_fn(template_fn=args.f, out_dir=out_dir)
+    gen_args_from_template_fn(template_fn=args.f, out_dir=out_dir, expand_split_dirs=True)
 
 
 if __name__ == "__main__":
@@ -64,4 +121,3 @@ if __name__ == "__main__":
 
     parsed_args = parser.parse_args()
     main(parsed_args)
-
